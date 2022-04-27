e77b0652269b21d0da1c77f404ab1c941e49786b
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-04-27 13:52:09 -0500

    started userlogin page

---
+++ userLogin.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+    <title>User Login</title>
+    <meta charset="UTF-8">
+</head>
+<?php
+    if(isset($_POST['submit'])){
+    try { // if something goes wrong, an exception is thrown
+        $dsn = "mysql:host=courses;dbname=z1905494";
+        $pdo = new PDO($dsn, 'z1905494', '2000Apr04');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $username = $_POST['username'];
+        $password = $_Post['password'];
+        
+        $sql = "SELECT USERNAME, PASSWORD
+                    FROM USERS
+                    WHERE USERNAME = '$username';";
+
+        $rs = $pdo->query($sql);
+        $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+
+        try{
+            foreach($row as $rows){
+                if($row[PASSWORD] == $password){
+                    ?>
+                    <body onload="document.forms[0].submit()">
+                    <form action="products.php" method="POST">
+                    <input type="hidden" name="username" value=""
+                    </form>
+                    <?
+                }
+                else{
+
+                }
+            }
+        }
+        catch{
+
+        }
+    }
+    catch(PDOexception $e) { // handle that exception
+        echo "Connection to database failed: " . $e->getMessage();
+    } 
+    }
+    else{
+?>
+    <body>
+    <form action="" method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Login">
+    </form>
+<?
+    }
+?> 
+</body>       
+</html>
