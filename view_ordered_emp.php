5e9a134b9d49fa5f250c5bd204dded279461db45
Quin Louke <z1943410@turing.cs.niu.edu>
2022-04-29 20:12:26 -0500

    started emp view of orders and fulfill page

---
+++ view_ordered_emp.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>View Order Status</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/color.css">
+</head>
+<body>
+<?php
+error_reporting(E_ALL);
+include('dblogininfo_TOM.php');
+try {
+    $dsn = "mysql:host=courses;dbname=$db_username";
+    $pdo = new PDO($dsn, $db_username, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $empid = isset($_POST['empid']) ? $_POST['empid'] : '';
+    $orderid = isset($_POST['orderid']) ? $_POST['orderid'] : '';
+    $fulfill = isset($_POST['fulfill']) ? $_POST['fulfill'] : '';
+
+    echo "EMPID = " . $empid . "</br>";
+    echo "ORDERID = " . $orderid . "</br>";
+
+    if ($fulfill) {
+        echo "Fulfill = " . $fulfill . "</br>";
+        $prepared = $pdo->prepare("INSERT INTO FULFILLMENT (ORDERID, EMPID, COMMENTS) VALUES (?, ?, ?);");
+        $prepared->execute([$orderid, $empid, $fulfill]);
+        echo "Successfully fulfilled the order.";
+    }
+    else if ($empid && $orderid){
+        $prepared = $pdo->prepare("SELECT * FROM ORDERS WHERE ORDERID = $orderid AND ORDERID NOT IN (SELECT ORDERid FROM FULFILLMENT);");
+        $prepared->execute();
+        $rows = $prepared->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($rows) {
+            draw_table($rows, 'ORDERS');
+            echo "Order comments";
+            echo "<form action=\"view_ordered_emp.php\" method=\"POST\">";
+                echo "<input type=\"hidden\" name=\"empid\" value=\"$empid\"/>";
+                echo "<input type=\"hidden\" name=\"orderid\" value=\"$orderid\"/>";
+                echo "<input type=\"text\" name=\"fulfill\"/>";
+                echo "<button type=\"submit\">Fulfill Order</button>";
+            echo "</form>";
+        }
+        else {
+            echo "This order has already been fulfilled.";
+        }
+    }
+    else {
+        echo "You are unauthorized to use this page.";
+    }
+}
+catch(PDOexception $e) {
+    echo "Connection to database failed: " . $e->getMessage();
+}
+function draw_table($rows, $table) {
+    echo "<table>";
+    // format display to specified table
+    echo "<th>" . "ORDERID" . "</th><th>" . "USERID" . "</th><th>" . "SADDRESS" . "</th><th>" . "SCITY" . "</th><th>" . "SSTATE" . "</th><th>" . "SZIP" . "</th><th>" . "TOTAL" . "</th><th>" . "ORDERSTATUS" . "</th><th>" . "TRACKINGID" . "</th>\n";
+    foreach($rows as $row) {
+        echo "<tr>";
+        echo "<td>" . $row["ORDERID"] . "</td><td>" . $row["USERID"] . "</td><td>" . $row["SADDRESS"] . "</td><td>" . $row["SCITY"] . "</td><td>" . $row["SSTATE"] . "</td><td>" . $row["SZIP"] . "</td><td>" . $row["TOTAL"] . "</td><td>" . $row["ORDERSTATUS"] . "</td><td>" . $row["TRACKINGID"] . "</td>\n";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
+</body>
+</html>
+<style>
---
+++ view_ordered_emp_list.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>View Order Status</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/color.css">
+</head>
+<body>
+<?php
+error_reporting(E_ALL);
+include('dblogininfo_TOM.php');
+try {
+    $dsn = "mysql:host=courses;dbname=$db_username";
+    $pdo = new PDO($dsn, $db_username, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $empid = isset($_POST['empid']) ? $_POST['empid'] : '';
+    $rows = '';
+
+    if ($empid) {
+        $prepared = $pdo->prepare("SELECT * FROM EMPLOYEES WHERE EMPID = $empid;");
+        $prepared->execute();
+        $rows = $prepared->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    if ($rows) {
+        echo "EMPID = " . $empid;
+
+        $prepared = $pdo->prepare("SELECT * FROM ORDERS WHERE ORDERID NOT IN (SELECT ORDERID FROM FULFILLMENT);");
+        $prepared->execute();
+        $rows = $prepared->fetchAll(PDO::FETCH_ASSOC);
+        draw_table($rows, 'ORDERS', $empid);
+    }
+    else {
+        echo "You are unauthorized to use this page.";
+    }
+}
+catch(PDOexception $e) {
+    echo "Connection to database failed: " . $e->getMessage();
+}
+function draw_table($rows, $table, $empid) {
+    echo "<table>";
+    // format display to specified table
+    echo "<th>" . "ORDERID" . "</th><th>" . "USERID" . "</th><th>" . "SADDRESS" . "</th><th>" . "SCITY" . "</th><th>" . "SSTATE" . "</th><th>" . "SZIP" . "</th><th>" . "TOTAL" . "</th><th>" . "ORDERSTATUS" . "</th><th>" . "TRACKINGID" . "</th><th></th>\n";
+    foreach($rows as $row) {
+        $oid = $row["ORDERID"];
+        echo "<tr>";
+        echo "<td>" . $oid . "</td><td>" . $row["USERID"] . "</td><td>" . $row["SADDRESS"] . "</td><td>" . $row["SCITY"] . "</td><td>" . $row["SSTATE"] . "</td><td>" . $row["SZIP"] . "</td><td>" . $row["TOTAL"] . "</td><td>" . $row["ORDERSTATUS"] . "</td><td>" . $row["TRACKINGID"] . "</td><td>";
+        echo "<form action=\"view_ordered_emp.php\" method=\"POST\">";
+            echo "<input type=\"hidden\" name=\"orderid\" value=\"$oid\"/>";
+            echo "<input type=\"hidden\" name=\"empid\" value=\"$empid\"/>";
+            echo "<button type=\"submit\">To Fulfillment Page.</button>";
+        echo "</form>";
+        echo "</td>\n";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
+</body>
+</html>
+<style>
