91fcd4165d502a0232d21debbbeb39154999a289
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-05-02 16:18:24 -0500

    userLogin complete need to connect to products page

--- userLogin.php
+++ userLogin.php
@@ -4,70 +4,68 @@
     <meta charset="UTF-8">
 </head>
 <?php
-    if(isset($_POST['submit'])){
+if(isset($_POST['submit'])){
     try { // if something goes wrong, an exception is thrown
         $dsn = "mysql:host=courses;dbname=z1905494";
         $pdo = new PDO($dsn, 'z1905494', '2000Apr04');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         $username = $_POST['username'];
-        $password = $_Post['password'];
+        $password = $_POST['password'];
         
-        $sql = "SELECT USERNAME, PASSWORD
+        $sql = "SELECT COUNT(USERNAME) FROM USERS WHERE USERNAME = '$username'";
+
+        $rs = $pdo->query($sql);
+        $row = $rs->fetch(PDO::FETCH_BOTH);
+
+        if($row[0] == 1){
+            $sql = "SELECT PASSWORD
                     FROM USERS
                     WHERE USERNAME = '$username';";
 
-        $rs = $pdo->query($sql);
-        $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+            $rs = $pdo->query($sql);
+            $rows = $rs->fetch(PDO::FETCH_BOTH);
 
-        try{
-            foreach($row as $rows){
-                if($row[PASSWORD] == $password){
-                    echo "<body onload=\"document.forms[0].submit()\">";
-                    echo "<form action=\"products.php\" method=\"POST\">";   #add action link to products page
-                    echo "<input type=\"hidden\" name=\"username\" value=\"$username\">";
-                    echo "<input tpye=\"hidden\" name=\"password\" value=\"$password\">"; 
-                    echo "</form>";
-                }
-                else{
-                ?>
-                    <body>
-                    <p>Username or Password Incorrect</p>
-                    <form action="" method="POST">
-                    <input type="text" name="username" placeholder="Username">
-                    <input type="text" name="password" placeholder="Password">
-                    <input type="submit" name="submit" value="Login">
-                    </form>
-                <?
-                }
+            if($rows[0] == $password){
+                echo "<body onload=\"document.forms[0].submit()\">";
+                echo "<form action=\"products.php\" method=\"POST\">";   #add action link to products page
+                echo "<input type=\"hidden\" name=\"username\" value=\"$username\">";
+                echo "<input tpye=\"hidden\" name=\"password\" value=\"$password\">"; 
+                echo "</form>";
+            }
+            else{
+                echo "<body>";
+                echo "<p>Password Incorrect</p>";
+                echo "<form action=\"\" method=\"POST\">";
+                echo "<input type=\"text\" name=\"username\" placeholder=\"Username\">";
+                echo "<input type=\"text\" name=\"password\" placeholder=\"Password\">";
+                echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
+                echo "</form>";
             }
         }
-        catch{
-        ?>
-            <body>
-            <p>Username or Password Incorrect</p>
-            <form action="" method="POST">
-            <input type="text" name="username" placeholder="Username">
-            <input type="text" name="password" placeholder="Password">
-            <input type="submit" name="submit" value="Login">
-            </form>
-        <?
-        }
+        else{
+            echo "<body>";
+            echo "<p>Username Incorrect</p>";
+            echo "<form action=\"\" method=\"POST\">";
+            echo "<input type=\"text\" name=\"username\" placeholder=\"Username\">";
+            echo "<input type=\"text\" name=\"password\" placeholder=\"Password\">";
+            echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
+            echo "</form>";
+       }
     }
     catch(PDOexception $e) { // handle that exception
-        echo "Connection to database failed: " . $e->getMessage();
-    } 
+        echo "Connection to database failed: " . $e->getMessage(); 
     }
-    else{
-?>
-    <body>
-    <form action="" method="POST">
-        <input type="text" name="username" placeholder="Username">
-        <input type="text" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Login">
-    </form>
-<?
-    }
+}
+else{
+    echo "<body>";
+    echo "<form action=\"\" method=\"POST\">";
+    echo "<input type=\"text\" name=\"username\" placeholder=\"Username\">";
+    echo "<input type=\"text\" name=\"password\" placeholder=\"Password\">";
+    echo "<input type=\"submit\" name=\"submit\" value=\"Login\">";
+    echo "</form>";
+
+}
 ?> 
 </body>       
 </html>
