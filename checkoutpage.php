70ac0e897ed259d5dcb9892720a5ae9b2d905729
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-05-03 10:31:03 -0500

    added more page to test html

--- checkoutpage.php
+++ checkoutpage.php
@@ -9,18 +9,48 @@
                 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                 
                 $userid = $_POST["userid"];
-                $sql = "SELECT *
-                        FROM CART
-                        WHERE USERID = $userid";
+                $sql = "SELECT PRODUCT.PRODNAME CART.QUANTITY 
+                        FROM CART USERS PRODUCT
+                        WHERE USERS.USERID = '$userid' AND CART.PRODID = PRODUCT.PRODID";
                 $rs = $pdo->query($sql);
-                $rows = $rs->fetchAll(PDO::FETCH_ASSOC); 
+                $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+                
+                echo "<table border=1 cellspacing=1>";
+                echo "<tr>";
+                foreach($rows[0] as $key => $item){
+                    echo "<th>$key</th>";
+                }
+                echo "</tr>";
+                foreach ($rows as $row) {
+                    echo "<tr>";
+                    foreach($row as $key => $item){
+                        echo "<td>$item</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
                 
                 echo "<p>Payment Information</p>";
                 $sql = "SELECT EMAIL, BADDRESS, BCITY, BSTATE, BZIP
                         FROM USERS
-                        WHERE USERNAME = $user";
+                        WHERE USERNAME = '$userid'";
                 $rs = $pdo->query($sql);
-                $rows = $rs->fetchAll(PDO::FETCH_ASSOC); 
+                $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+                
+                echo "<table border=1 cellspacing=1>";
+                echo "<tr>";
+                foreach($rows[0] as $key => $item){
+                    echo "<th>$key</th>";
+                }
+                echo "</tr>";
+                foreach ($rows as $row) {
+                    echo "<tr>";
+                    foreach($row as $key => $item){
+                        echo "<td>$item</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
             }
             catch(PDOexception $e) { // handle that exception
                 echo "Connection to database failed: " . $e->getMessage();
