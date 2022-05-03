f4cb6752d059b7e6f08a06e90fb113e5f79d4640
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-05-03 16:40:20 -0500

    finished checkout page need to fix up database side

--- checkoutpage.php
+++ checkoutpage.php
@@ -9,12 +9,15 @@
                 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                 
                 $userid = $_POST["userid"];
-                $sql = "SELECT PRODUCT.PRODNAME CART.QUANTITY 
-                        FROM CART USERS PRODUCT
-                        WHERE USERS.USERID = '$userid' AND CART.PRODID = PRODUCT.PRODID";
+                $sql = "SELECT PRODUCTS.PRODNAME, CART.QUANTITY, PRODUCTS.PRICE 
+                        FROM CART, USERS, PRODUCTS
+                        WHERE USERS.USERID = '$userid'";
                 $rs = $pdo->query($sql);
                 $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+                $total = 0;
+                $rowTotal = 0; 
                 
+                echo "<p>Cart</p>";
                 echo "<table border=1 cellspacing=1>";
                 echo "<tr>";
                 foreach($rows[0] as $key => $item){
@@ -23,17 +26,24 @@
                 echo "</tr>";
                 foreach ($rows as $row) {
                     echo "<tr>";
-                    foreach($row as $key => $item){
-                        echo "<td>$item</td>";
-                    }
+                    echo "<td>$row[PRODNAME]</td>";
+                    echo "<td>$row[QUANTITY]</td>";
+                    $rowtotal = $row['QUANTITY'] * $row['PRICE'];
+                    $total = $total + $rowtotal;
+                    echo "<td>\$$rowtotal</td>";
                     echo "</tr>";
                 }
+                    echo "<tr>";
+                    echo "<th>TOTAL</th>";
+                    echo "<td> </td>";
+                    echo "<th>\$$total</th>";
+                    echo "</tr>";
                 echo "</table>";
                 
                 echo "<p>Payment Information</p>";
                 $sql = "SELECT EMAIL, BADDRESS, BCITY, BSTATE, BZIP
                         FROM USERS
-                        WHERE USERNAME = '$userid'";
+                        WHERE USERID = '$userid'";
                 $rs = $pdo->query($sql);
                 $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
                 
@@ -51,11 +61,20 @@
                     echo "</tr>";
                 }
                 echo "</table>";
-            }
+                
+                echo "<p>Enter Shipping Address and Credit Card</p>";
+                echo "<form action=\"createorder.php\" method=\"POST\">";
+                echo "<input type=\"text\" name=\"card\" placeholder=\"Credit Card\"><br>";
+                echo "<input type=\"text\" name=\"address\" placeholder=\"Address\">";
+                echo "<input type=\"text\" name=\"city\" placeholder=\"City\"><br>";
+                echo "<input type=\"text\" name=\"state\" placeholder=\"State\">";
+                echo "<input type=\"text\" name=\"zip\" placeholder=\"Zip Code\"><br>";
+                echo "<input type=\"submit\" name=\"submit\" value=\"Place Order\">";
+                echo "</form>";
+            }     
             catch(PDOexception $e) { // handle that exception
                 echo "Connection to database failed: " . $e->getMessage();
             }
-
         ?>
     </pre></body>
 </html>
