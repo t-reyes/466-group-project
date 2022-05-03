d817cde1dc11ff7b4d3a32f3afec67cee2ead4e6
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-05-03 17:27:27 -0500

    finished and connected between checkout and cust orders

--- createorder.php
+++ createorder.php
@@ -13,9 +13,22 @@ try {
     $scity = $_POST['city'];
     $sstate = $_POST['state'];
     $szip = $_POST['zip'];
+    $total = $_POST['total'];
     $tracking = rand(1,99999999);
 
+    $sql = "INSERT INTO ORDERS
+                (USERID, SADDRESS, SCITY, SSTATE, SZIP, TOTAL, ORDERSTATUS, TRACKINGID)
+                VALUES ('$userid', '$saddress', '$scity', '$sstate', '$szip', '$total', 'PROCESSING', '$tracking')";
     
+    $pdo->exec($sql);
+
+    $sql = "DELETE FROM CART WHERE USERID = '$userid';"
+
+    $pdo->exec($sql);
+
+    echo "<form action=\"view_ordered_cust.php\" method=\"POST\">";
+    echo "<input type=\"hidden\" name=\"userid\" value=\"$userid\">";
+    echo "</form>";
 }
 catch(PDOexception $e) { // handle that exception
     echo "Connection to database failed: " . $e->getMessage();
