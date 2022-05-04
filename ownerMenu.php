72bc11939ca0c990f6fecc79cbbf2e74412daace
Thomas Reyes <z1905494@hopper.cs.niu.edu>
2022-05-04 10:16:15 -0500

    added back button

--- ownerMenu.php
+++ ownerMenu.php
@@ -7,26 +7,29 @@
                 $dsn = "mysql:host=courses;dbname=$db_username";
                 $pdo = new PDO($dsn, $db_username, $db_password);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+                $empid = $_POST['empid'];
                 
                 echo "<form action=\"employeeList.php\" method=\"POST\">";
                 echo "<input type=\"submit\" value=\"Employee List\">";
+                echo "<input type=\"hidden\" name=\"empid\" value=\"$empid\">";
                 echo "</form>";
 
                 echo "<form action=\"ownerInventory.php\" method=\"POST\">";
                 echo "<input type=\"submit\" value=\"Inventory\">";
+                echo "<input type=\"hidden\" name=\"empid\" value=\"$empid\">";
                 echo "</form>";
 
                 echo "<form action=\"view_ordered_emp_list.php\" method=\"POST\">";
                 echo "<input type=\"submit\" value=\"Orders\">";
+                echo "<input type=\"hidden\" name=\"empid\" value=\"$empid\">";
                 echo "</form>";
-
-
-
-
             }     
             catch(PDOexception $e) { // handle that exception
                 echo "Connection to database failed: " . $e->getMessage();
             }
         ?>
-    </pre></body>
+    </pre>
+<a href="employeeLogin.php">Log out</a>
+</body>
 </html>
